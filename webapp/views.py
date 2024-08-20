5f63449343463971c0b6c5ce07c79436af19154f
nowshedahmed <[nowshedahmed59@gmail.com]>
2024-08-20 16:48:01 +0600

    html page upload

--- webapp/views.py
+++ webapp/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-from django.http import HttpResponse
-
-# Create your views here.
-
 
 
 
 def home(request):
-    return HttpResponse("<center><h1>Welcome to First Django deployment on Vercel")
+    return render(request, 'index.html')
 
 
